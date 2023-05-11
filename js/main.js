d94a6c49dd9f60b9000e1fb56a65ce1816e5b3f4
Sulbaran Huamani <85169754+sulbaranhuamani@users.noreply.github.com>
2023-05-11 17:19:03 -0500

    h17-18d11m05a2023

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+//js de burguer menu
+/*
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
+
+var line1__bars = document.querySelector(".line1__bars-menu");
+var line2__bars = document.querySelector(".line2__bars-menu");
+var line3__bars = document.querySelector(".line3__bars-menu");
+
+function animateBars(){
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+}
+*/
+
 //
+const menu = document.querySelector("#menu");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+
+open.addEventListener("click", ()=>{
+    menu.classList.add("visible")
+});
+close.addEventListener("click", ()=>{
+    menu.classList.remove("visible")
+});
